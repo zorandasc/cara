e92cae5fd7af71212892cb37f2a219eddb6b9fad
Zoran Dasic <zorandsc@yahoo.com>
2021-01-12 23:41:11 +0100

    mala ispravka bakanalija

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,44 +1,162 @@
-import React from 'react';
+import React from "react"
 
 import Divider from "../elements/divider"
-import SVG from "./SVG"
+import SVG from "./Svg"
 import { UpDown, UpDownWide } from "../styles/animations"
 
 const Hero = ({ offset, factor = 1 }) => {
-    return (
-        <div>
-            <Divider speed={0.2} offset={offset} factor={factor}>
+  return (
+    <div>
+      <Divider speed={0.2} offset={offset} factor={factor}>
+        <UpDown>
+          <SVG
+            icon="triangle"
+            width={48}
+            stroke
+            color="icon_orange"
+            left="10%"
+            top="20%"
+          ></SVG>
+          <SVG
+            icon="hexa"
+            width={48}
+            stroke
+            color="icon_red"
+            left="60%"
+            top="70%"
+          ></SVG>
+          <SVG
+            icon="box"
+            width={20}
+            color="icon_green"
+            left="60%"
+            top="15%"
+          ></SVG>
+        </UpDown>
+        <UpDownWide>
+          <SVG
+            icon="arrowUp"
+            width={16}
+            color="icon_blue"
+            left="80%"
+            top="10%"
+          />
+          <SVG
+            icon="triangle"
+            width={12}
+            stroke
+            color="icon_brightest"
+            left="90%"
+            top="50%"
+          />
+          <SVG
+            icon="circle"
+            width={16}
+            color="icon_darker"
+            left="70%"
+            top="90%"
+          />
+          <SVG
+            icon="triangle"
+            width={16}
+            stroke
+            color="icon_darkest"
+            left="30%"
+            top="65%"
+          />
+          <SVG
+            icon="cross"
+            width={16}
+            stroke
+            color="icon_pink"
+            left="28%"
+            top="15%"
+          />
+          <SVG
+            icon="circle"
+            width={6}
+            color="icon_darkest"
+            left="75%"
+            top="10%"
+          />
+          <SVG
+            icon="upDown"
+            width={8}
+            color="icon_darkest"
+            left="45%"
+            top="10%"
+          />
+        </UpDownWide>
+        <SVG
+          icon="circle"
+          hiddenMobile
+          width={24}
+          color="icon_darker"
+          left="5%"
+          top="70%"
+        />
+        <SVG icon="circle" width={6} color="icon_darkest" left="4%" top="20%" />
+        <SVG
+          icon="circle"
+          width={12}
+          color="icon_darkest"
+          left="50%"
+          top="60%"
+        />
+        <SVG
+          icon="upDown"
+          width={8}
+          color="icon_darkest"
+          left="95%"
+          top="90%"
+        />
+        <SVG
+          icon="upDown"
+          hiddenMobile
+          width={24}
+          color="icon_darker"
+          left="40%"
+          top="80%"
+        />
+        <SVG
+          icon="triangle"
+          width={8}
+          stroke
+          color="icon_darker"
+          left="25%"
+          top="5%"
+        />
+        <SVG icon="circle" width={64} color="icon_green" left="95%" top="5%" />
+        <SVG
+          icon="box"
+          hiddenMobile
+          width={64}
+          color="icon_purple"
+          left="5%"
+          top="90%"
+        />
+        <SVG icon="box" width={6} color="icon_darkest" left="10%" top="10%" />
+        <SVG icon="box" width={12} color="icon_darkest" left="40%" top="30%" />
+        <SVG
+          icon="hexa"
+          width={16}
+          stroke
+          color="icon_darker"
+          left="10%"
+          top="50%"
+        />
+        <SVG
+          icon="hexa"
+          width={8}
+          stroke
+          color="icon_darker"
+          left="80%"
+          top="70%"
+        />
+      </Divider>
+      <h1>HERO</h1>
+    </div>
+  )
+}
 
-                <UpDown>
-                    <SVG icon="triangle" width={48} stroke color="icon_orange" left="10%" top="20%"></SVG>
-                    <SVG icon="hexa" width={48} stroke color="icon_red" left="60%" top="70%"></SVG>
-                    <SVG icon="box" width={20} color="icon_green" left="60%" top="15%"></SVG>
-                </UpDown>
-                <UpDownWide>
-                    <SVG icon="arrowUp" width={16} color="icon_blue" left="80%" top="10%" />
-                    <SVG icon="triangle" width={12} stroke color="icon_brightest" left="90%" top="50%" />
-                    <SVG icon="circle" width={16} color="icon_darker" left="70%" top="90%" />
-                    <SVG icon="triangle" width={16} stroke color="icon_darkest" left="30%" top="65%" />
-                    <SVG icon="cross" width={16} stroke color="icon_pink" left="28%" top="15%" />
-                    <SVG icon="circle" width={6} color="icon_darkest" left="75%" top="10%" />
-                    <SVG icon="upDown" width={8} color="icon_darkest" left="45%" top="10%" />
-                </UpDownWide>
-                <SVG icon="circle" hiddenMobile width={24} color="icon_darker" left="5%" top="70%" />
-                <SVG icon="circle" width={6} color="icon_darkest" left="4%" top="20%" />
-                <SVG icon="circle" width={12} color="icon_darkest" left="50%" top="60%" />
-                <SVG icon="upDown" width={8} color="icon_darkest" left="95%" top="90%" />
-                <SVG icon="upDown" hiddenMobile width={24} color="icon_darker" left="40%" top="80%" />
-                <SVG icon="triangle" width={8} stroke color="icon_darker" left="25%" top="5%" />
-                <SVG icon="circle" width={64} color="icon_green" left="95%" top="5%" />
-                <SVG icon="box" hiddenMobile width={64} color="icon_purple" left="5%" top="90%" />
-                <SVG icon="box" width={6} color="icon_darkest" left="10%" top="10%" />
-                <SVG icon="box" width={12} color="icon_darkest" left="40%" top="30%" />
-                <SVG icon="hexa" width={16} stroke color="icon_darker" left="10%" top="50%" />
-                <SVG icon="hexa" width={8} stroke color="icon_darker" left="80%" top="70%" />
-            </Divider>
-            <h1>HERO</h1>
-        </div>
-    );
-};
-
-export default Hero;
+export default Hero
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,44 +1,125 @@
-import React from 'react';
+import React from "react"
 
 import Divider from "../elements/divider"
-import SVG from "./SVG"
+import SVG from "./Svg"
 import { UpDown, UpDownWide } from "../styles/animations"
 
 const Projects = ({ offset, factor = 2 }) => {
-    return (
-        <div>
-            <Divider
-                offset={1}
-                factor={factor}
-                speed={-0.2}
-                bg="linear-gradient(to right, #c31432, #240b36)"
-                clipPath="polygon(0 15%, 100% 25%, 100% 85%, 0 75%)"
-            >
-            </Divider>
-            <Divider speed={0.1} offset={offset} factor={factor}>
-                <UpDown>
-                    <SVG icon="box" width={6} color="icon_brightest" left="85%" top="75%" />
-                    <SVG icon="upDown" width={8} color="icon_teal" left="70%" top="20%" />
-                    <SVG icon="triangle" width={8} stroke color="icon_orange" left="25%" top="5%" />
-                    <SVG icon="circle" hiddenMobile width={24} color="icon_blue" left="17%" top="60%" />
-                </UpDown>
-                <UpDownWide>
-                    <SVG icon="arrowUp" hiddenMobile width={16} color="icon_green" left="20%" top="90%" />
-                    <SVG icon="triangle" width={12} stroke color="icon_brightest" left="90%" top="30%" />
-                    <SVG icon="circle" width={16} color="icon_yellow" left="70%" top="90%" />
-                    <SVG icon="triangle" hiddenMobile width={16} stroke color="icon_teal" left="18%" top="75%" />
-                    <SVG icon="circle" width={6} color="icon_brightest" left="75%" top="10%" />
-                    <SVG icon="upDown" hiddenMobile width={8} color="icon_green" left="45%" top="10%" />
-                </UpDownWide>
-                <SVG icon="circle" hiddenMobile width={6} color="icon_brightest" left="4%" top="20%" />
-                <SVG icon="circle" width={12} color="icon_pink" left="80%" top="60%" />
-                <SVG icon="box" width={6} color="icon_orange" left="10%" top="10%" />
-                <SVG icon="box" width={12} color="icon_yellow" left="29%" top="26%" />
-                <SVG icon="hexa" width={16} stroke color="icon_red" left="75%" top="30%" />
-                <SVG icon="hexa" width={8} stroke color="icon_yellow" left="80%" top="70%" />
-            </Divider>
-        </div >
-    );
-};
+  return (
+    <div>
+      <Divider
+        offset={1}
+        factor={factor}
+        speed={-0.2}
+        bg="linear-gradient(to right, #c31432, #240b36)"
+        clipPath="polygon(0 15%, 100% 25%, 100% 85%, 0 75%)"
+      ></Divider>
+      <Divider speed={0.1} offset={offset} factor={factor}>
+        <UpDown>
+          <SVG
+            icon="box"
+            width={6}
+            color="icon_brightest"
+            left="85%"
+            top="75%"
+          />
+          <SVG icon="upDown" width={8} color="icon_teal" left="70%" top="20%" />
+          <SVG
+            icon="triangle"
+            width={8}
+            stroke
+            color="icon_orange"
+            left="25%"
+            top="5%"
+          />
+          <SVG
+            icon="circle"
+            hiddenMobile
+            width={24}
+            color="icon_blue"
+            left="17%"
+            top="60%"
+          />
+        </UpDown>
+        <UpDownWide>
+          <SVG
+            icon="arrowUp"
+            hiddenMobile
+            width={16}
+            color="icon_green"
+            left="20%"
+            top="90%"
+          />
+          <SVG
+            icon="triangle"
+            width={12}
+            stroke
+            color="icon_brightest"
+            left="90%"
+            top="30%"
+          />
+          <SVG
+            icon="circle"
+            width={16}
+            color="icon_yellow"
+            left="70%"
+            top="90%"
+          />
+          <SVG
+            icon="triangle"
+            hiddenMobile
+            width={16}
+            stroke
+            color="icon_teal"
+            left="18%"
+            top="75%"
+          />
+          <SVG
+            icon="circle"
+            width={6}
+            color="icon_brightest"
+            left="75%"
+            top="10%"
+          />
+          <SVG
+            icon="upDown"
+            hiddenMobile
+            width={8}
+            color="icon_green"
+            left="45%"
+            top="10%"
+          />
+        </UpDownWide>
+        <SVG
+          icon="circle"
+          hiddenMobile
+          width={6}
+          color="icon_brightest"
+          left="4%"
+          top="20%"
+        />
+        <SVG icon="circle" width={12} color="icon_pink" left="80%" top="60%" />
+        <SVG icon="box" width={6} color="icon_orange" left="10%" top="10%" />
+        <SVG icon="box" width={12} color="icon_yellow" left="29%" top="26%" />
+        <SVG
+          icon="hexa"
+          width={16}
+          stroke
+          color="icon_red"
+          left="75%"
+          top="30%"
+        />
+        <SVG
+          icon="hexa"
+          width={8}
+          stroke
+          color="icon_yellow"
+          left="80%"
+          top="70%"
+        />
+      </Divider>
+    </div>
+  )
+}
 
-export default Projects;
+export default Projects
